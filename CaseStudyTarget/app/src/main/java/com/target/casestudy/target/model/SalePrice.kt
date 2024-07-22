e91888af57c01be442f28aaecf01253f705309d2
shilpancs <59900397+shilpancs@users.noreply.github.com>
2024-07-22 05:40:13 +0530

    Delete CaseStudyTarget/app/src/main/java/com/target/casestudy/target/model/SalePrice.kt

--- CaseStudyTarget/app/src/main/java/com/target/casestudy/target/model/SalePrice.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.caseStudy.model
-
-import java.io.Serializable
-
-data class SalePrice(
-    val amount_in_cents: Int,
-    val currency_symbol: String,
-    val display_string: String?
-): Serializable
